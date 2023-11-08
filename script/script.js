f396ccf66a2dcd15afbfdb48106212e885480929
Rob Martin <59516575+RobCMYK@users.noreply.github.com>
2023-11-08 17:00:24 -0600

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -10,19 +10,19 @@ $(document).ready(function ()
     e.preventDefault();
     
     var userFirstName = $("#firstname").val(); //defines inputString as the user's input value
-    document.getElementById("#userData").innerHTML = "Your first name: " + userFirstName; //add user data to text area
+    document.getElementById("#userData").innerText = "Your first name: " + userFirstName; //add user data to text area
     
     var userLastName = $("#lastname").val(); 
-    document.getElementById("#userData").innerHTML = "Your last name: " + userLastName; 
+    document.getElementById("#userData").innerText = "Your last name: " + userLastName; 
 
     var userEmail = $("#email").val(); 
-    document.getElementById("#userData").innerHTML = "Your email: " + userEmail;
+    document.getElementById("#userData").innerText = "Your email: " + userEmail;
 
     var userPassword = $("#pwd").val(); 
-    document.getElementById("#userData").innerHTML = "Your password: " + userPassword;    
+    document.getElementById("#userData").innerText = "Your password: " + userPassword;    
     
     var userBDay = $("#bDate").val(); 
-    document.getElementById("#userData").innerHTML = "Your birthday: " + userBDay;
+    document.getElementById("#userData").innerText = "Your birthday: " + userBDay;
 
     });
 
