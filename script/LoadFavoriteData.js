492b7306b359e0860be5bd897f833cef2c216ceb
Rob Martin <59516575+RobCMYK@users.noreply.github.com>
2023-11-07 20:40:23 -0600

    Add files via upload

---
+++ script/LoadFavoriteData.js
@@ -0,0 +1,42 @@
+$('#btnLoadData').click(function() {
+    console.log("clicked");
+
+//     let jsonURL = "https://www.w3schools.com/jquery/demo_ajax_json.js";
+
+//     let jsonURL = "demo.json";
+
+    let jsonURL = "https://barrycumbie.github.io/376-india-lab/demo.json";
+
+    $.ajax({
+        url: jsonURL,
+        dataType: "json",
+        success: function(data) {
+            //can log either the entire data or invoke specific properties data.zipCode...
+            console.log(data.firstName);
+            
+            //loads first name into my first input box (in example #1)
+            $("#favoriteData").val(data.firstName);
+            console.log(data.language);
+
+            $('input[type=checkbox]').each(function () {
+                if (data.language === $(this).val())
+                {
+                  $(this).prop('checked', true)
+                    console.log($(this));
+               }
+
+            });
+            //console.log (sList);
+
+            $.each(data, function (key, val) {
+                //step through results
+                console.log(key, val);
+                //get each data item
+                //put it in the right place
+                //$("#dataContainer").append(key + " " + val);
+                $(`#${key}`).val(val);
+                
+            });
+        }
+    });
+});
